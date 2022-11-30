b1eb57eb664460da2475096a8e2be743ecfc3965
LukeRivia <95368707+LukeRivia@users.noreply.github.com>
2022-11-30 15:42:47 -0300

    Arquivo

--- Beecrowd - 1060.py
+++ Beecrowd - 1060.py
@@ -3,4 +3,4 @@ for y in range(6):                          # Para cada item até 6
     a = float(input())                      # Lê valor flutuante
     if a > 0:                               # a sendo maior que 0
         positivo += 1                       # Positivo recebe maior
-print(f"{positivo} valores positivos")      # Exibe mensagem apos for ser concluído   
+print(f"{positivo} valores positivos")      # Exibe mensagem apos for ser concluído
